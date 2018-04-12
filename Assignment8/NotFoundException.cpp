883a14c9b8b4173ed0a7740c3b0f6c62843fd363
jesse r <jerodarte@gmail.com>
2018-04-11 19:39:19 -0700

    First Commit

---
+++ Assignment8/NotFoundException.cpp
@@ -0,0 +1,15 @@
+//  Created by Frank M. Carrano and Tim Henry.
+//  Copyright (c) 2013 __Pearson Education__. All rights reserved.
+
+/** @file NotFoundException.cpp */
+#include "stdafx.h"
+
+#include "NotFoundException.h"  
+
+NotFoundException::NotFoundException(const string& message)
+         : logic_error("Precondition Violated Exception: " + message)
+{
+}  // end constructor
+
+// End of implementation file.
+
